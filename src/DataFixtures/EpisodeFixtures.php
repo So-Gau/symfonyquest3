08ec4f47e9507e665ca4c904bf86e6dfb7168120
So-Gau <sophie.gaubert10@gmail.com>
2022-01-10 17:22:56 +0100

    add new episode in episodefixtures and make:user

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -15,12 +15,22 @@ class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
     public const EPISODES = [
         [
-            'title'=> 'The Walking Dead',
+            'title'=> 'Passée Décomposé',
             'number'=> '1',
             'synopsis'=>'Rick Grimes, adjoint au shérif, sombre dans le coma après avoir reçu une balle dans l abdomen.
              Lorsqu il se réveille à l hôpital, il découvre avec stupéfaction que des morts-vivants peuplent la ville. Aussitôt, 
              il se précipite chez lui et constate que sa femme Lori et son fils Carl ont disparu…',
-            'season'=> 'season_1',
+            'season'=> 'season_0',
+        ],
+        [
+            'title'=> 'Tripes',
+            'number'=> '2',
+            'synopsis'=>'Rick parvient à s échapper du tank grâce à l aide de Glenn, dont il avait entendu la voix à la radio.
+            Rick et Glenn se réunissent avec les compagnons de Glenn, un autre groupe de survivants dont Andrea, T-dog, Merle, Morales, Jacqui, 
+            venus pour se ravitailler au supermarché. Cependant, l arrivée mouvementée de Rick les met tous en péril, 
+            l attention des zombies ayant été attirée sur leur cachette. Assiégé par les zombies, le groupe parvient brièvement à communiquer par radio avec le groupe de Shane et Lori,
+            mais ceux-ci décident qu ils ne peuvent les aider, la présence de Rick ne leur ayant pas encore été communiquée.,',
+            'season'=> 'season_0',
         ],
     ];
 
