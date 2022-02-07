475b4ba0fe31a297713b12d98a68d4540408d6dc
So-Gau <sophie.gaubert10@gmail.com>
2022-02-07 10:41:17 +0100

    add username for register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -33,7 +33,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('_wdt');
+            return $this->redirectToRoute('program_index');
         }
 
         return $this->render('registration/register.html.twig', [
