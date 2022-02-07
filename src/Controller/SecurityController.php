7be5e4b31a1c7d1fe2c731d118b3715a3d0a7e26
So-Gau <sophie.gaubert10@gmail.com>
2022-02-07 11:27:14 +0100

    replace registration controller with securitycontroller and modify routes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RegistrationController extends AbstractController
+class SecurityController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
@@ -36,8 +36,8 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('program_index');
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
+        return $this->render('security/register.html.twig', [
+            'securityForm' => $form->createView(),
         ]);
     }
 }
